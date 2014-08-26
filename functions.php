f13deef642549d0866e06a9524c34afa466caee0
norboo <istrate.paul@gmail.com>
2014-08-26 15:36:39 +0800

    functions.php and style.css changes

--- functions.php
+++ functions.php
@@ -14,4 +14,20 @@ function load_cornerstone_child_scripts() {
 
 add_action('wp_enqueue_scripts', 'load_cornerstone_child_scripts',50);
 
+//Remove Orbit
+function remove_orbit () {
+
+remove_action('init', 'Orbit');
+
+}
+add_action('after_setup_theme', 'remove_orbit');
+
+// Add pages to query. Show Author archive when no posts
+function custom_post_author_archive($query) {
+    if ($query->is_author)
+        $query->set( 'post_type', array('post', 'page','about') );
+    remove_action( 'pre_get_posts', 'custom_post_author_archive' );
+}
+add_action('pre_get_posts', 'custom_post_author_archive');
+
 ?>
